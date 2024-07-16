54fcea321884db32ce6244f97a6f67e6889170b7
Jamshid-coder2705 <70112803+Jamshid-coder2705@users.noreply.github.com>
2024-07-16 23:31:28 +0500

    Refactoring

--- PhoneBook/Services/ShowPrinter.cs
+++ PhoneBook/Services/ShowPrinter.cs
@@ -1,9 +1,5 @@
 ﻿using PhoneBook.Interface;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhoneBook.Services
 {
